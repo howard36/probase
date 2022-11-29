ce12f7a981cf52d682775a23bef769115a000179
Howard Halim <howardhalim@gmail.com>
2022-11-29 17:32:15 -0500

    create next app

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Probase</title>
+        <meta name="description" content="A math contest problem database" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div className="text-6xl">
+          Hello World
+        </div>
+      </main>
+    </div>
+  )
+}
