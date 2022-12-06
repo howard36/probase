b56e105959ccacb66f2b559f1f47d32eb1970ab4
Howard Halim <howardhalim@gmail.com>
2022-12-06 00:41:42 -0500

    Add sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,23 @@
 export default function Sidebar() {
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <div class="flex flex-col w-64 h-screen px-4 py-8 bg-white">
+      <h2 class="text-3xl font-semibold text-center text-slate-800">Probase</h2>
+      
+      <div class="flex flex-col justify-between flex-1 mt-6">
+        <nav>
+          <a class="flex items-center px-4 py-2 my-4 text-slate-600 transition-colors duration-300 rounded-lg hover:bg-slate-100 hover:text-slate-700" href="#">
+            <span class="mx-4 font-medium">Home</span>
+          </a>
+
+          <a class="flex items-center px-4 py-2 my-4 text-slate-700 bg-slate-100 rounded-lg" href="#">
+            <span class="mx-4 font-medium">CMIMC</span>
+          </a>
+
+          <a class="flex items-center px-4 py-2 my-4 text-slate-600 transition-colors duration-300 rounded-lg hover:bg-slate-100 hover:text-slate-700" href="#">
+            <span class="mx-4 font-medium">HMMT</span>
+          </a>
+        </nav>
+      </div>
     </div>
   );
 }
