3ccbd0691b7ca0bc2aac0fc7a3668286f86d1beb
Howard Halim <howardhalim@gmail.com>
2023-02-07 03:07:00 -0500

    functional submission page

--- pages/api/c/[cid]/new.tsx
+++ pages/api/c/[cid]/new.tsx
@@ -1,19 +1,35 @@
-import { insertOne } from '@/utils/mongodb';
+import { insertOne, findOne } from '@/utils/mongodb';
+import { ObjectId } from 'mongodb';
 
 export default function handler(req, res) {
   // console.log(req);
   const { cid } = req.query;
   if (req.method === 'POST') {
     // Process a POST request
-    const title = req.body.title;
-    const statement = req.body.statement;
-    const problem = {
-      title,
-      statement,
-    };
-    insertOne("problems", {
-      document: problem,
-    });
+    const { title, statement, subject, answer, solution } = req.body;
+
+    findOne('collections', {
+      filter: { cid },
+      projection: { '_id': 1 },
+    }).then(collection => {
+      const id = new ObjectId(collection._id)
+      // TODO: assign PID based on existing problems
+      const pid = 'A3';
+      const problem = {
+        pid,
+        title,
+        subject,
+        statement,
+        answer,
+        solutions: [solution],
+        authors: [],
+        collection_id: id,
+      };
+      console.log(problem);
+      insertOne("problems", {
+        document: problem,
+      }).then(_ => console.log("done"));
+    })
   } else {
     // Handle any other HTTP method
   }
--- pages/c/[cid]/new.tsx
+++ pages/c/[cid]/new.tsx
@@ -58,8 +58,25 @@ export default function Collection({ collection }: Props) {
               <input id="title" name="title" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
             </div>
             <div className="relative mb-4">
+              <label className="leading-7 text-md text-gray-600">Subject</label>
+              <select id="subject" name="subject" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                <option value="Algebra">Algebra</option>
+                <option value="Combinatorics">Combinatorics</option>
+                <option value="Geometry">Geometry</option>
+                <option value="Number Theory">Number Theory</option>
+              </select>
+            </div>
+            <div className="relative mb-4">
               <label className="leading-7 text-md text-gray-600">Problem Statement</label>
               <textarea id="statement" name="statement" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+            </div>
+            <div className="relative mb-4">
+              <label className="leading-7 text-md text-gray-600">Answer</label>
+              <input id="answer" name="answer" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
+            </div>
+            <div className="relative mb-4">
+              <label className="leading-7 text-md text-gray-600">Solution</label>
+              <textarea id="solution" name="solution" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
             </div>
             <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
           </form>
