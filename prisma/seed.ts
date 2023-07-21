e3133ba5ad134637dffb52b794f95e4dd548efe9
Howard Halim <howardhalim@gmail.com>
2023-07-20 22:08:28 -0400

    prisma seed db

---
+++ prisma/seed.ts
@@ -0,0 +1,124 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const cmimc = await prisma.collection.upsert({
+    where: { cid: 'cmimc' },
+    update: {},
+    create: {
+      name: 'CMIMC',
+      cid: 'cmimc',
+      showAuthors: false,
+    }
+  });
+
+  const howardUser = await prisma.user.upsert({
+    where: { email: 'howardhalim@gmail.com' },
+    update: {},
+    create: {
+      name: 'Howard Halim blah',
+      email: 'howardhalim@gmail.com',
+    }
+  });
+
+  await prisma.permission.upsert({
+    where: {
+      userId_collectionId: {
+        userId: howardUser.id,
+        collectionId: cmimc.id,
+      }
+    },
+    update: {},
+    create: {
+      userId: howardUser.id,
+      collectionId: cmimc.id,
+      accessLevel: 'Admin',
+    }
+  });
+
+  const howardAuthor = await prisma.author.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      displayName: 'Howard Halim',
+      collectionId: cmimc.id,
+    }
+  });
+
+  await prisma.problem.upsert({
+    where: {
+      collectionId_pid: {
+        collectionId: cmimc.id,
+        pid: 'A1',
+      }
+    },
+    update: {},
+    create: {
+      collectionId: cmimc.id,
+      pid: 'A1',
+      title: 'Quadratic Equation',
+      statement: 'Find all roots of $$x^2 - 4x + 2$$',
+      submitterId: howardUser.id,
+      authors: {
+        connect: [{ id: howardAuthor.id }]
+      },
+      answer: String.raw`$2 \pm \sqrt{2}$`,
+      solutions: {
+        create: [{
+          text: String.raw`Use the quadratic formula to get
+$$x = \frac{4 \pm \sqrt{4^2 - 4 \cdot 1 \cdot 2}}{2} = 2 \pm \sqrt{2}$$`,
+          authors: {
+            connect: [{ id: howardAuthor.id }]
+          },
+        }]
+      },
+      subject: 'Algebra',
+      difficulty: 0,
+      isAnonymous: false,
+    },
+  });
+
+  await prisma.problem.upsert({
+    where: {
+      collectionId_pid: {
+        collectionId: cmimc.id,
+        pid: 'N1',
+      }
+    },
+    update: {},
+    create: {
+      collectionId: cmimc.id,
+      pid: 'N1',
+      title: 'Diophantine',
+      statement: 'Find all positive integer solutions to $$a^n + b^n = c^n.$$',
+      submitterId: howardUser.id,
+      authors: {
+        connect: [{ id: howardAuthor.id }]
+      },
+      answer: String.raw`No positive integer solutions for $n \ge 3$.`,
+      solutions: {
+        create: [{
+          text: 'Try using LTE.',
+          authors: {
+            connect: [{ id: howardAuthor.id }]
+          },
+        }]
+      },
+      subject: 'NumberTheory',
+      difficulty: 0,
+      isAnonymous: false,
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  })
