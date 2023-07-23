f4f563ca5554bd1e446eba1327cc850ee5075139
Howard Halim <howardhalim@gmail.com>
2023-07-23 14:37:38 -0400

    gradients for each subject

--- app/c/[cid]/p/[pid]/problem-page.tsx
+++ app/c/[cid]/p/[pid]/problem-page.tsx
@@ -3,6 +3,26 @@ import Statement from './statement'
 import Spoilers from './spoilers'
 import type { CollectionProps, ProblemProps } from './types'
 
+// darker color first, for more contrast
+const subjectToGradient = {
+  'Algebra': {
+    subject: 'Algebra',
+    gradient: 'from-sky-500 to-cyan-500'
+  },
+  'Combinatorics': {
+    subject: 'Combinatorics',
+    gradient: 'from-amber-500 to-yellow-500'
+  },
+  'Geometry': {
+    subject: 'Geometry',
+    gradient: 'from-emerald-500 to-green-500'
+  },
+  'NumberTheory': {
+    subject: 'Number Theory',
+    gradient: 'from-violet-500 to-purple-500'
+  },
+}
+
 export default function ProblemPage({
   problem,
   collection,
@@ -15,6 +35,8 @@ export default function ProblemPage({
     written_by = <p className="italic text-slate-700 text-base mb-8 text-right">Written by {problem.authors[0].displayName}</p>;
   }
 
+  let { subject, gradient } = subjectToGradient[problem.subject];
+
   return (
     <div className="p-12 sm:py-24 text-slate-800 whitespace-pre-wrap break-words hyphens-auto">
       {/* fixed width container, matching ideal 60-character line length */}
@@ -22,8 +44,8 @@ export default function ProblemPage({
         <div className="text-2xl sm:text-3xl text-slate-900 font-bold mb-4">
           <Title problem={problem} collection={collection} />
         </div>
-        <div className={`py-2 px-6 inline-block mb-4 text-slate-50 font-semibold text-sm text-center leading-none rounded-full bg-gradient-to-r from-amber-500 to-yellow-500`}>
-          Combinatorics
+        <div className={`py-2 px-6 inline-block mb-4 text-slate-50 font-semibold text-sm text-center leading-none rounded-full bg-gradient-to-r ${gradient}`}>
+          {subject}
         </div>
         <div className="mb-4">
           <Statement problem={problem} collection={collection} />
