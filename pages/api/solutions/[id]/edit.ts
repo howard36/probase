d76068e087a94f5e6e2f20560b032cad5a75672e
Howard Halim <howardhalim@gmail.com>
2023-07-24 22:18:16 -0400

    try-catch in solution edit API

--- pages/api/solutions/[id]/edit.ts
+++ pages/api/solutions/[id]/edit.ts
@@ -3,70 +3,84 @@ import prisma from '@/utils/prisma';
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../../auth/[...nextauth]'
 import { canEditSolution } from '@/utils/permissions'
+import { handleApiError } from '@/utils/error';
 
-// TODO: add permissions for API
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // TODO: why can't this be PUT? Something about CORS
   if (req.method !== 'POST') {
-    res.status(405).json({
+    return res.status(405).json({
       error: {
         message: 'Invalid method'
       }
     });
-    return;
   }
 
   const session = await getServerSession(req, res, authOptions);
   if (!session) {
-    res.status(401).json({
+    return res.status(401).json({
       error: {
         message: 'Not signed in'
       }
     });
-    return;
   }
 
-  // Get solution
-  const solutionId = parseInt(req.query.id as string);
-  const solution = await prisma.solution.findUnique({
-    where: { id: solutionId },
-    select: {
-      authors: {
-        select: { id: true }
-      },
-      problem: {
-        select: {
-          collection: {
-            select: { id: true }
+  const idString = req.query.id as string;
+  if (!Number.isInteger(idString)) {
+    return res.status(400).json({
+      error: {
+        message: 'ID must be an integer'
+      }
+    });
+  }
+
+  try {
+    // Get solution
+    const solutionId = parseInt(idString);
+    const solution = await prisma.solution.findUnique({
+      where: { id: solutionId },
+      select: {
+        authors: {
+          select: { id: true }
+        },
+        problem: {
+          select: {
+            collection: {
+              select: { id: true }
+            }
           }
         }
       }
+    });
+
+    if (solution === null) {
+      return res.status(404).json({
+        error: {
+          message: `No solution with id ${solutionId}`
+        }
+      });
     }
-  });
 
-  if (solution === null) {
-    res.status(404).json({
-      error: {
-        message: `No solution with id ${solutionId}`
+    if (!canEditSolution(session, solution, solution.problem.collection)) {
+      return res.status(403).json({
+        error: {
+          message: 'You do not have permission to edit this solution'
+        }
+      });
+    }
+
+    const { text, summary } = req.body;
+
+    await prisma.solution.update({
+      where: { id: solutionId },
+      data: {
+        text,
+        summary,
       }
-    });
-    return;
+    })
+
+    res.status(200).json({'message': 'Successfully updated'});
+  } catch (error) {
+    handleApiError(error, res);
   }
-
-  if (!canEditSolution(session, solution, solution.problem.collection)) {
-    res.status(403).json({
-      error: {
-        message: 'You do not have permission to edit this solution'
-      }
-    });
-    return;
-  }
-
-  // TODO: multiple authors per solution
-  await prisma.solution.update({
-    where: { id: solutionId },
-    data: req.body,
-  })
-
-  res.status(200).json({'msg': 'updated'});
 }
 
