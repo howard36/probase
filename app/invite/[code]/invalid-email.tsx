fe084d4d5238b17f23aa3fc2214d17a23a5d5733
Howard Halim <howardhalim@gmail.com>
2023-08-21 02:53:34 -0400

    fix: assume user.name always exists

--- app/invite/[code]/invalid-email.tsx
+++ app/invite/[code]/invalid-email.tsx
@@ -16,7 +16,7 @@ export default function InvalidEmail({
   return (
     <div className="p-8 text-slate-800 whitespace-pre-wrap break-words">
       <div className="sm:w-144 mx-auto my-12 sm:my-24">
-        {inviterName && <h1 className="text-3xl mb-6 font-bold text-slate-900">{inviterName} invited you!</h1>}
+        <h1 className="text-3xl mb-6 font-bold text-slate-900">{inviterName} invited you!</h1>
         <p className="text-xl mb-6">Log in with an <span className="font-bold text-slate-900">@{emailDomain}</span> email to join <span className="font-bold text-slate-900">{collectionName}</span></p>
         <p className="text-xl mb-16">Currently logged in as <span className="font-bold text-slate-900">{email}</span></p>
         <GoogleLoginButton />
