bb51a2b1e275cb2620ebac008bad205fe394c0ec
Howard Halim <howardhalim@gmail.com>
2023-08-25 10:22:42 -0400

    refactor: promise-can-edit in new file

---
+++ app/c/[cid]/p/[pid]/promise-can-edit.ts
@@ -0,0 +1,39 @@
+import { authOptions } from "@/api/auth/[...nextauth]";
+import prisma from "@/utils/prisma";
+import { getServerSession } from "next-auth";
+import { CollectionProps, ProblemProps } from "./types";
+import { canEditProblem2 } from "@/utils/permissions";
+
+export async function promiseCanEditProblem(
+  problem: ProblemProps,
+  collection: CollectionProps,
+): Promise<boolean> {
+  const session = await getServerSession(authOptions);
+  if (session === null) {
+    return false;
+  }
+
+  const userId = session.userId;
+  if (userId === undefined) {
+    throw new Error("userId is undefined despite being logged in");
+  }
+
+  // TODO: parallel db requests
+  const collectionId = collection.id;
+  const permission = await prisma.permission.findUnique({
+    where: {
+      userId_collectionId: {
+        userId,
+        collectionId,
+      }
+    }
+  });
+  const authors = await prisma.author.findMany({
+    where: {
+      userId,
+      collectionId,
+    },
+    select: { id: true },
+  });
+  return canEditProblem2(problem, permission, authors);
+}
--- app/c/[cid]/p/[pid]/title.tsx
+++ app/c/[cid]/p/[pid]/title.tsx
@@ -1,41 +1,7 @@
 import EditableTitle from './editable-title'
 import Latex from '@/components/latex'
 import type { CollectionProps, ProblemProps } from './types'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/api/auth/[...nextauth]'
-import prisma from '@/utils/prisma'
-import { canEditProblem2 } from '@/utils/permissions'
-
-async function promiseCanEdit(problem: ProblemProps, collection: CollectionProps): Promise<boolean> {
-  const session = await getServerSession(authOptions);
-  if (session === null) {
-    return false;
-  }
-
-  const userId = session.userId;
-  if (userId === undefined) {
-    throw new Error("userId is undefined despite being logged in");
-  }
-
-  // TODO: parallel db requests
-  const collectionId = collection.id;
-  const permission = await prisma.permission.findUnique({
-    where: {
-      userId_collectionId: {
-        userId,
-        collectionId,
-      }
-    }
-  });
-  const authors = await prisma.author.findMany({
-    where: {
-      userId,
-      collectionId,
-    },
-    select: { id: true },
-  });
-  return canEditProblem2(problem, permission, authors);
-}
+import { promiseCanEditProblem } from './promise-can-edit'
 
 export default async function Title({
   problem,
@@ -44,12 +10,11 @@ export default async function Title({
   problem: ProblemProps
   collection: CollectionProps
 }) {
-  const canEdit = await promiseCanEdit(problem, collection);
+  const canEdit = await promiseCanEditProblem(problem, collection);
 
   if (canEdit) {
     return <EditableTitle problem={problem} />;
   } else {
-    // TODO: is p needed here?
-    return <p><Latex>{`${problem.title}`}</Latex></p>;
+    return <Latex>{`${problem.title}`}</Latex>;
   }
 }
