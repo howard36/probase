afe441e958df278f9043a1a73c1d4ae858dbabe5
Howard Halim <howardhalim@gmail.com>
2023-11-26 14:19:40 -0500

    Give up button

--- app/c/[cid]/p/[pid]/testsolve.tsx
+++ app/c/[cid]/p/[pid]/testsolve.tsx
@@ -13,8 +13,9 @@ export default function Testsolve({problem, solveAttempt, deadline}: {problem: P
   const [answer, setAnswer] = useState('');
   const [wrongAnswer, setWrongAnswer] = useState('');
   const [isSubmitting, setSubmitting] = useState(false);
+  const [isGivingUp, setGivingUp] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSubmitting(true);
 
@@ -38,19 +39,39 @@ export default function Testsolve({problem, solveAttempt, deadline}: {problem: P
     }
     setSubmitting(false);
   };
+  
+  const handleGiveUp = async () => {
+    setGivingUp(true);
+
+    await fetch(`/api/problems/${problem.id}/testsolve/give-up`, {
+      method: 'POST',
+      cache: 'no-store',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({}),
+    });
+
+    router.refresh();
+    setGivingUp(false);
+  };
 
   return <div>
     <form onSubmit={handleSubmit} className="mb-8">
       <Label text="ANSWER" />
-      <div className="mb-4">
+      <div className="mb-3">
         <AimeInput value={answer} onValueChange={(newValue: string) => setAnswer(newValue)} required />
       </div>
-      <div className="flex gap-x-6 items-center">
+      <div className="flex gap-x-6 items-center my-4">
         {/* TODO: make this a component */}
-        <button disabled={isSubmitting || answer === ''} className={`text-white text-lg font-bold rounded border-0 py-2 w-40 flex-grow-0 ${answer === '' ? "bg-slate-300" : "bg-blue-500 hover:bg-blue-600"} focus:outline-none flex flex-auto items-center justify-center`}>
+        <button disabled={isSubmitting || answer === ''} type="submit" className={`text-white text-lg font-bold rounded border-0 py-2 w-40 flex-grow-0 ${answer === '' ? "bg-slate-300" : "bg-blue-500 hover:bg-blue-600"} focus:outline-none flex flex-auto items-center justify-center`}>
           { isSubmitting && <div className="animate-spin rounded-full border-solid border-blue-400 border-l-blue-50 border-4 h-6 w-6 mr-3 inline-block"></div> }
           { isSubmitting ? "Saving..." : "Submit" }
         </button>
+        <button disabled={isGivingUp} type="button" onClick={handleGiveUp} className={`text-red-500 hover:text-red-600 text-lg font-bold rounded border-0 py-2 w-40 flex-grow-0 bg-red-100 hover:bg-red-200 focus:outline-none flex flex-auto items-center justify-center`}>
+          { isGivingUp && <div className="animate-spin rounded-full border-solid border-blue-400 border-l-blue-50 border-4 h-6 w-6 mr-3 inline-block"></div> }
+          { isGivingUp ? "Saving..." : "Give up" }
+        </button>
+      </div>
+      <div className="mb-3">
         {wrongAnswer && <span><strong>{wrongAnswer}</strong> is incorrect!</span>}
       </div>
     </form>
