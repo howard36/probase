7deb56b21c3bda4da604ea1af9d2dcf763feea43
Howard Halim <howardhalim@gmail.com>
2023-11-26 17:58:52 -0500

    Escape apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <Sidebar>
       <div className="p-4 pt-8 sm:p-24 text-base sm:text-lg">
         <h1 className="text-3xl font-semibold mb-4">Welcome to Probase!</h1>
-        <p className="mb-3">There's not much here yet, but you can check out the <Link href="/demo" prefetch={true} className="text-sky-600 hover:underline">demo</Link> or the <Link href="https://github.com/howard36/probase" prefetch={true} className="text-sky-600 hover:underline">Github repo</Link>.</p>
+        <p className="mb-3">There&apos;s not much here yet, but you can check out the <Link href="/demo" prefetch={true} className="text-sky-600 hover:underline">demo</Link> or the <Link href="https://github.com/howard36/probase" prefetch={true} className="text-sky-600 hover:underline">Github repo</Link>.</p>
         <p>There are also links to private collections on the left, if you have access to them.</p>
       </div>
     </Sidebar>
