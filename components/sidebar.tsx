ef07db399559ce1a81e2bf864e9d8fe33396bf59
Howard Halim <howardhalim@gmail.com>
2023-11-26 18:01:18 -0500

    Otis link (#50)
    * Add home page text and OTIS link in sidebar
    
    * Responsive sidebar, part 1
    
    * Responsive sidebar, part 2
    
    * Escape apostrophe

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -16,25 +16,25 @@ export default function Sidebar({
   const links = [
     { href: '/c/demo', label: 'Demo', active: (pathname !== null) && pathname.startsWith('/c/demo') },
     { href: '/c/cmimc', label: 'CMIMC', active: (pathname !== null) && pathname.startsWith('/c/cmimc') },
-    { href: '/c/cmo', label: 'CMO', active: (pathname !== null) && pathname.startsWith('/c/cmo') },
+    { href: '/c/otis-mock-aime', label: 'OTIS Mock AIME', active: (pathname !== null) && pathname.startsWith('/c/otis-mock-aime') },
   ];
 
   return (
     <>
-      <div className="fixed w-64 left-0 top-0 h-screen flex flex-col px-6 py-6 bg-white overflow-y-auto soft-shadow-r-lg" aria-label="Sidenav">
-        <h2 className="text-3xl font-bold text-center text-slate-900 mt-8 mb-4">Probase</h2>
+      <div className="fixed w-40 sm:w-64 left-0 top-0 h-screen flex flex-col p-4 sm:p-6 pt-12 bg-white overflow-y-auto soft-shadow-r-lg" aria-label="Sidenav">
+        <h2 className="text-2xl sm:text-3xl font-bold px-3 sm:text-center text-slate-900 mb-4">Probase</h2>
         
         <div className="flex flex-col justify-between flex-1">
           <nav>
             {links.map(({ href, label, active }) => (
-              <Link key={href} href={href} prefetch={true} className={`flex items-center p-3 px-6 my-2 rounded-lg ${active ? "bg-slate-100 text-slate-700" : "text-slate-600 hover:bg-slate-100 hover:text-slate-700 transition-colors duration-300"}`}>
+              <Link key={href} href={href} prefetch={true} className={`flex items-center px-3 py-3 sm:px-6 my-2 rounded-lg ${active ? "bg-slate-100 text-slate-700" : "text-slate-600 hover:bg-slate-100 hover:text-slate-700 transition-colors duration-300"}`}>
                 <span className="font-medium">{label}</span>
               </Link>
             ))}
           </nav>
         </div>
       </div>
-      <div className="ml-64">
+      <div className="ml-40 sm:ml-64">
         {children}
       </div>
     </>
