35386dae13e1c68285076caa4155fdea4d53e090
Howard Halim <howardhalim@gmail.com>
2023-12-01 18:11:48 -0500

    Use Inter as default font (#65)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import { Metadata } from 'next'
 import Providers from './providers'
+import { Inter } from 'next/font/google'
 
 import '../styles/globals.css'
 import 'katex/dist/katex.min.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
+import { cn } from '@/utils/utils'
 config.autoAddCss = false
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Probase',
@@ -18,12 +25,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="bg-slate-50">
+    <html lang="en">
       {/*
       <link rel="icon" href="/favicon.ico" />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css" />
       */}
-      <body>
+      <body className={cn("bg-slate-50", inter.className)}>
         <Providers>{children}</Providers>
       </body>
     </html>
