79790bb64495ff5f9da0e1ede3eafe9694862830
Howard Halim <howardhalim@gmail.com>
2023-12-01 17:33:29 -0500

    Update button component (#64)

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -32,7 +32,9 @@ export default function SubmitButton({
       className={cn(
         "rounded-lg text-white font-semibold border-0 inline-flex items-center justify-center",
         size === "sm" ? "gap-x-1.5 py-2 px-2.5 text-base" : "gap-x-3 py-2 px-4 text-lg",
-        "bg-sky-500 hover:bg-sky-600 active:bg-sky-700 shadow shadow-sky-500/20 hover:shadow-md hover:shadow-sky-500/20 focus:outline-none focus-visible:ring-2 focus-visible:ring-sky-300 focus-visible:ring-offset-2 focus-visible:ring-offset-slate-50 active:shadow active:shadow-sky-500/20 disabled:bg-sky-300 disabled:pointer-events-none disabled:cursor-progress transition-colors ease-in-out duration-150",
+        "shadow hover:shadow-md active:shadow focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-slate-50 disabled:pointer-events-none",
+        "transition-colors ease-in-out duration-150",
+        "bg-sky-500 hover:bg-sky-600 active:bg-sky-700 shadow-sky-500/20 hover:shadow-sky-500/20  focus-visible:ring-sky-300 active:shadow-sky-500/20 disabled:bg-sky-300",
         className
       )}
       {...props}
