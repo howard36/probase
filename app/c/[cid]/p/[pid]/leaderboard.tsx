0937f80f0534fc26f15321556a394eec9d819b0c
Howard Halim <howardhalim@gmail.com>
2023-12-12 03:14:52 -0500

    show total solves and total attempts

--- app/c/[cid]/p/[pid]/leaderboard.tsx
+++ app/c/[cid]/p/[pid]/leaderboard.tsx
@@ -64,24 +64,24 @@ export default function Testsolve({
   });
 
   // Non-admin users are restricted to only seeing the top 3
-  const numTop = permission?.accessLevel === "Admin" ? numAttempts : Math.max(3, numSolved);
+  const numTop = permission?.accessLevel === "Admin" ? numAttempts : Math.min(3, numSolved);
   const visibleEntries = entries.filter((entry, idx) => idx < numTop || entry.highlight)
 
   return (
-    <div className="my-8">
+    <div className="my-12">
       <h2 className="mb-4 text-lg lg:text-2xl font-bold text-slate-900">Leaderboard</h2>
-      <table className="min-w-full divide-y divide-gray-200">
-        <tbody className="bg-white divide-y divide-gray-200">
+      <table className="min-w-full">
+        <tbody className="">
           {visibleEntries.map((entry, idx) => (
             <tr key={idx} className={entry.highlight ? "bg-yellow-100" : ""}>
-              <td className="pr-3 py-3 whitespace-nowrap text-right">{entry.rank}.</td>
+              <td className="pr-3 py-3 whitespace-nowrap text-right w-12">{entry.rank}.</td>
               <td className="py-3 whitespace-nowrap font-semibold">{entry.name}</td>
               <td className="py-3 whitespace-nowrap text-red-500">
                 {entry.numFailed > 0 &&
                   <span><FontAwesomeIcon icon={faTimes} /> {entry.numFailed}</span>
                 }
               </td>
-              <td className="pr-6 py-3 whitespace-nowrap text-green-500">
+              <td className="py-3 whitespace-nowrap text-green-500">
                 {entry.solveTimeMillis &&
                   <span><FontAwesomeIcon icon={faCheck} /> {formatTime(entry.solveTimeMillis)}</span>
                 }
@@ -90,6 +90,7 @@ export default function Testsolve({
           ))}
         </tbody>
       </table>
+      <p className="my-4 text-slate-700 text-base">{numSolved} out of {numAttempts} testsolvers solved this problem{numTop < numSolved && ` (showing top ${numTop})`}</p>
     </div>
   );
 }
