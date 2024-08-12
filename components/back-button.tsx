a0dfac701311718d2eb6664dc8ec144bb0cf739e
Howard Halim <howardhalim@gmail.com>
2024-08-12 12:55:04 -0400

    Fix back button (#115)

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function BackButton({
   href,
@@ -9,27 +10,54 @@ export default function BackButton({
   href: string;
   label: string;
 }) {
-  return (
-    <Link
-      href={href}
-      prefetch={true}
-      className="text-slate-600 hover:text-slate-800 underline flex items-center"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
+  const router = useRouter();
+
+  if (typeof window !== "undefined" && window.history.length > 1) {
+    return (
+      <button
+        onClick={() => router.back()}
+        className="text-slate-600 hover:text-slate-800 underline flex items-center"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M15.75 19.5L8.25 12l7.5-7.5"
-        />
-      </svg>
-      <span className="ml-1">{label}</span>
-    </Link>
-  );
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+        <span className="ml-1">{label}</span>
+      </button>
+    );
+  } else {
+    return (
+      <Link
+        href={href}
+        prefetch={true}
+        className="text-slate-600 hover:text-slate-800 underline flex items-center"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+        <span className="ml-1">{label}</span>
+      </Link>
+    );
+  }
 }
