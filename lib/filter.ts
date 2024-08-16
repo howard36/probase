d3429931faaa48920171265822c72547f83e6808
Howard Halim <howardhalim@gmail.com>
2024-08-15 23:51:34 -0400

    Save search query in URL (#117)
    * Add search to Filter type
    
    * Save new search in URL
    
    * Move page to last in URL
    
    * Remove router.back in back button

--- lib/filter.ts
+++ lib/filter.ts
@@ -10,31 +10,38 @@ const allSubjects: Subject[] = [
 export type Filter = {
   page: number;
   subjects: Subject[];
+  search: string;
 };
 
 // TODO: allow arbitrary JSON as input
 export function parseFilter(searchParams: {
   page?: string;
   subject?: string;
+  search?: string;
 }): Filter {
   const page = parseInt(searchParams.page ?? "1", 10) || 1;
   const subjectString = searchParams.subject || "";
   const subjects = allSubjects.filter((subject) =>
     subjectString.toLowerCase().includes(subject[0].toLowerCase()),
   );
-  return { page, subjects };
+  const search = searchParams.search || "";
+  return { page, subjects, search };
 }
 
+// TODO: escape special characters
 export function filterToString(filter: Filter): string {
   const queryParams = new URLSearchParams();
-  if (filter.page !== undefined && filter.page !== 1) {
-    queryParams.set("page", filter.page.toString());
-  }
   if (filter.subjects !== undefined && filter.subjects.length > 0) {
     const subjectString = filter.subjects
       .map((s) => s[0].toLowerCase())
       .join("");
     queryParams.set("subject", subjectString);
+  }
+  if (filter.search !== "") {
+    queryParams.set("search", filter.search);
+  }
+  if (filter.page !== undefined && filter.page !== 1) {
+    queryParams.set("page", filter.page.toString());
   }
   const queryString = queryParams.toString();
   return queryString ? `?${queryString}` : "";
