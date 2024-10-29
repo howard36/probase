a0b8a8fa199a510cf95c10678d3b2772084b6c06
Howard Halim <howardhalim@gmail.com>
2024-10-29 08:54:27 -0400

    Install shadcn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,14 +1,6 @@
-import { ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function isNonNegativeInt(s: string) {
-  return /^\d+$/.test(s);
-}
-
-export function sleep(seconds: number) {
-  return new Promise((resolve) => setTimeout(resolve, 1000 * seconds));
 }
