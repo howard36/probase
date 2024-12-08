4c5d43eec8a7a6908065972a4b8fcb4c5114347b
Howard Halim <howardhalim@gmail.com>
2024-12-08 01:44:19 -0500

    New checkbox and switch components (#148)
    * Add checkboxes
    
    * shadcn switches
    
    * background and primary color, use Icon suffix
    
    * Adjust layout

--- components/problem-list-filter.tsx
+++ components/problem-list-filter.tsx
@@ -7,6 +7,9 @@ import {
 } from "@prisma/client";
 import { Filter, filterToString } from "@/lib/filter";
 import { usePathname, useRouter } from "next/navigation";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 
 const allSubjects: Subject[] = [
   "Algebra",
@@ -49,49 +52,38 @@ export function ProblemListFilter({
   };
 
   return (
-    <div className="mb-4 flex flex-row justify-between xl:flex-col">
-      <div className="form-control">
+    <div className="my-6 flex flex-col justify-between gap-y-4">
+      <div className="flex flex-col gap-y-2">
         {allSubjects.map((subject) => (
-          <label key={subject} className="label cursor-pointer justify-start">
-            <input
-              type="checkbox"
+          <div key={subject} className="flex items-center">
+            <Checkbox
+              id={`filter-subject-${subject}`}
               checked={filter.subjects.includes(subject)}
-              onChange={() => toggleSubject(subject)}
-              className="checkbox-primary checkbox [--chkfg:white]"
+              onCheckedChange={() => toggleSubject(subject)}
             />
-            <span className="label-text ml-2 text-sm font-medium text-slate-600">
-              {subject}
-            </span>
-          </label>
+            <Label htmlFor={`filter-subject-${subject}`}>
+              {subject === "NumberTheory" ? "Number Theory" : subject}
+            </Label>
+          </div>
         ))}
       </div>
-      <div className="form-control ml-auto xl:ml-0">
-        <label className="label cursor-pointer justify-start gap-x-2">
-          <input
-            type="checkbox"
-            className="toggle toggle-primary"
-            checked={filter.archived}
-            onChange={toggleArchived}
-          />
-          <span className="label-text whitespace-nowrap text-sm font-medium text-slate-600">
-            Archived
-          </span>
-        </label>
+      <div className="flex items-center">
+        <Switch
+          id="filter-archived"
+          checked={filter.archived}
+          onCheckedChange={toggleArchived}
+        />
+        <Label htmlFor="filter-archived">Archived</Label>
       </div>
       {collection.requireTestsolve &&
         permission?.testsolverType === TestsolverType.Serious && (
-          <div className="form-control ml-auto xl:ml-0">
-            <label className="label cursor-pointer justify-start gap-x-2">
-              <input
-                type="checkbox"
-                className="toggle toggle-primary"
-                checked={filter.unsolvedOnly}
-                onChange={toggleUnsolvedOnly}
-              />
-              <span className="label-text whitespace-nowrap text-sm font-medium text-slate-600">
-                Unsolved only
-              </span>
-            </label>
+          <div className="flex items-center">
+            <Switch
+              id="filter-unsolved"
+              checked={filter.unsolvedOnly}
+              onCheckedChange={toggleUnsolvedOnly}
+            />
+            <Label htmlFor="filter-unsolved">Unsolved only</Label>
           </div>
         )}
     </div>
