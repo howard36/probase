f78fe5fafa1d7f7934dfe68400e2b4557fef2042
Howard Halim <howardhalim@gmail.com>
2024-12-08 00:49:58 -0500

    shadcn switches

--- components/problem-list-filter.tsx
+++ components/problem-list-filter.tsx
@@ -9,6 +9,7 @@ import { Filter, filterToString } from "@/lib/filter";
 import { usePathname, useRouter } from "next/navigation";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 
 const allSubjects: Subject[] = [
   "Algebra",
@@ -56,41 +57,31 @@ export function ProblemListFilter({
         {allSubjects.map((subject) => (
           <div key={subject} className="flex items-center">
             <Checkbox
-              id={subject}
+              id={`filter-subject-${subject}`}
               checked={filter.subjects.includes(subject)}
               onCheckedChange={() => toggleSubject(subject)}
             />
-            <Label htmlFor={subject}>{subject}</Label>
+            <Label htmlFor={`filter-subject-${subject}`}>{subject}</Label>
           </div>
         ))}
       </div>
-      <div className="form-control ml-auto xl:ml-0">
-        <label className="label cursor-pointer justify-start gap-x-2">
-          <input
-            type="checkbox"
-            className="toggle toggle-primary"
-            checked={filter.archived}
-            onChange={toggleArchived}
-          />
-          <span className="label-text whitespace-nowrap text-sm font-medium text-slate-600">
-            Archived
-          </span>
-        </label>
+      <div className="flex items-center">
+        <Switch
+          id="filter-archived"
+          checked={filter.archived}
+          onCheckedChange={toggleArchived}
+        />
+        <Label htmlFor="filter-archived">Archived</Label>
       </div>
       {collection.requireTestsolve &&
         permission?.testsolverType === TestsolverType.Serious && (
-          <div className="form-control ml-auto xl:ml-0">
-            <label className="label cursor-pointer justify-start gap-x-2">
-              <input
-                type="checkbox"
-                className="toggle toggle-primary"
-                checked={filter.unsolvedOnly}
-                onChange={toggleUnsolvedOnly}
-              />
-              <span className="label-text whitespace-nowrap text-sm font-medium text-slate-600">
-                Unsolved only
-              </span>
-            </label>
+          <div className="flex items-center">
+            <Switch
+              id="filter-unsolved"
+              checked={filter.unsolvedOnly}
+              onCheckedChange={toggleUnsolvedOnly}
+            />
+            <Label htmlFor="filter-unsolved">Unsolved only</Label>
           </div>
         )}
     </div>
